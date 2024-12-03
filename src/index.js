f07dc063ad686622554989cfd47a2b0840a30aff
KuzAnton8335 <ant2783@gmail.com>
2024-12-03 14:37:53 +0300

    без JSX

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+// import { App } from './App';
 import './index.css';
-// import { MiniApp } from './miniapp';
+import { MiniApp } from './miniapp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<MiniApp />
 	</React.StrictMode>,
 );
